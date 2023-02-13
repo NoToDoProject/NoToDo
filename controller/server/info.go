a75f9c58e3e7cb7c3e9a430fe5d5aba08a0d50a6
AkagiYui <ddio001@163.com>
2023-02-13 20:19:24 +0800

    feat: add more information in server info api

--- controller/server/info.go
+++ controller/server/info.go
@@ -3,10 +3,20 @@ package server
 import (
 	"github.com/NoToDoProject/NoToDo/common/response"
 	"github.com/gin-gonic/gin"
+	"time"
+)
+
+var (
+	StartTimestamp = time.Now().Unix() // 服务器启动时间戳
 )
 
 // Information 服务器信息
 func Information(c *gin.Context) {
 	nc := response.ContextEx{Context: c}
-	nc.Success()
+	nc.Success(gin.H{
+		"appName":     "NoToDo",                           // 应用名称
+		"currentTime": time.Now().Unix(),                  // 当前时间戳
+		"startTime":   StartTimestamp,                     // 服务器启动时间戳
+		"uptime":      time.Now().Unix() - StartTimestamp, // 服务器运行时间
+	})
 }
